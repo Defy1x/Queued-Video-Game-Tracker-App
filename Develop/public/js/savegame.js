39b481e8d3578814fd84f28b46b8078af003d1a6
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-11 19:49:04 -0400

    updated UI and handlebars

---
+++ Develop/public/js/savegame.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('#game-form').submit(function(e){
+    e.preventDefault();
+    game = $('#game-name').val().toLowerCase();
+
+    console.log(game);
+
+    // if no value entered for game then exit
+    if (!game){
+      return;
+    }
+
+    //show search details on home page and render results
+    document.location.assign( `/search-results?search=${game}` );
+
+    //clear game form value
+    $('#game-name').val('');
+  })
+})
