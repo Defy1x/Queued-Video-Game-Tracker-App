60c124606de7394855e407c0f92130859177cafc
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-12 10:34:02 -0400

    committing all new changed

--- Develop/models/Game.js
+++ Develop/models/Game.js
@@ -26,50 +26,62 @@ Game.init(
         gameSummary:{
           type: DataTypes.TEXT,
           allowNull: true,
+          defaultValue: "N/A"
         },
         gameArtwork: {
             type: DataTypes.TEXT,
             allowNull: true,
+            defaultValue: "N/A"
           },
         gamePlatform: {
             type: DataTypes.TEXT,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gameGenre: {
               type: DataTypes.TEXT,
               allowNull: true,
+              defaultValue: "N/A"
           },
           gameTag: {
               type: DataTypes.TEXT,
               allowNull: true,
+              defaultValue: "N/A"
             },
           gameESRB: {
               type: DataTypes.STRING,
               allowNull: true,
+              defaultValue: "N/A"
           },
         gameReleased: {
             type: DataTypes.STRING,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gameRating:{
             type: DataTypes.INTEGER,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gamePublisher:{
             type: DataTypes.TEXT,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gameDeveloper:{
             type: DataTypes.TEXT,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gameStatus:{
             type: DataTypes.INTEGER,
             allowNull: true,
+            defaultValue: "N/A"
           },
           gameWebsite:{
             type: DataTypes.STRING,
             allowNull: true,
+            defaultValue: "N/A"
           }
       },
     {
