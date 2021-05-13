d363dc8bb0a52a29dd67d47843de2f8cc3238169
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-13 14:17:01 -0400

    updates to helpers and UI

--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -1,7 +1,13 @@
 module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    formatTime: function(date){
+    const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    dateStyle: "short",
+    timeStyle: "long"
+    };
   },
   format_amount: (amount) => {
     // format large numbers with commas
